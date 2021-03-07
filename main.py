6d6f10a39be07cff2ae718de2db1dd4fbb92c8fd
jarsba <jarkko.lehtiranta@gmail.com>
2021-03-08 00:12:54 +0200

    Get React working with Flask

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from auth import authenticate, identity
 import os
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from flask import Flask, url_for, jsonify
-from flask import redirect, render_template, request
+from flask import Flask, url_for, jsonify, Blueprint
+from flask import redirect, render_template, request, send_from_directory
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.middleware.shared_data import SharedDataMiddleware
 from werkzeug.wrappers import Response
 from flask_jwt import JWT, jwt_required, current_identity
 from flask_bcrypt import generate_password_hash
 from flask_bcrypt import Bcrypt
 from datetime import timedelta
-
 
 from models import User
 from models import ChatRoom
@@ -26,9 +26,14 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
+# CONSTANT
+
+PATH_PREFIX = '/api'
+
 # APP
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='/')
+
 CORS(app)
 app.debug = os.getenv('FLASK_ENV') != 'production'
 
@@ -63,16 +68,11 @@ Session = sessionmaker(bind=db)
 # JWT
 
 app.config["SECRET_KEY"] = os.getenv("SC_SECRET_KEY")
-app.config["JWT_AUTH_URL_RULE"] = "/login"
+app.config["JWT_AUTH_URL_RULE"] = "/api/login"
 app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=21600)
 bcrypt = Bcrypt(app)
 
 jwt = JWT(app, authenticate, identity)
-
-app.wsgi_app = DispatcherMiddleware(
-    Response('Not Found', status=404),
-    {'/api': app.wsgi_app}
-)
 
 
 def render_response(data="", status_code=200, message=""):
@@ -83,7 +83,17 @@ def render_response(data="", status_code=200, message=""):
     })
 
 
-@app.route("/user", methods=['GET'])
+print(app.url_map)
+
+
+# For React
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return app.send_static_file('index.html')
+
+
+@app.route(f"{PATH_PREFIX}/user", methods=['GET'])
 @jwt_required()
 def get_all_users():
     logger.info("Get all users")
@@ -92,7 +102,7 @@ def get_all_users():
     return render_response(data=users)
 
 
-@app.route("/user/<id>", methods=['GET'])
+@app.route(f"{PATH_PREFIX}/user/<id>", methods=['GET'])
 @jwt_required()
 def get_user(id):
     logger.info("Get a user")
@@ -101,7 +111,7 @@ def get_user(id):
     return render_response(data=user)
 
 
-@app.route("/user", methods=['POST'])
+@app.route(f"{PATH_PREFIX}/user", methods=['POST'])
 @jwt_required()
 def create_user():
     logger.info("Create a user")
@@ -119,7 +129,7 @@ def create_user():
     return render_response(data=new_user)
 
 
-@app.route("/user/<id>", methods=['DELETE'])
+@app.route(f"{PATH_PREFIX}/user/<id>", methods=['DELETE'])
 @jwt_required()
 def delete_user(id):
     logger.info("Delete a user")
@@ -131,7 +141,8 @@ def delete_user(id):
 
     return render_response()
 
-@app.route("/user/<id>", methods=['PUT'])
+
+@app.route(f"{PATH_PREFIX}/user/<id>", methods=['PUT'])
 @jwt_required()
 def update_user(id):
     logger.info("Update a user")
@@ -160,7 +171,7 @@ def update_user(id):
     return render_response(data=user)
 
 
-@app.route("/chatroom", methods=['GET'])
+@app.route(f"{PATH_PREFIX}/chatroom", methods=['GET'])
 @jwt_required()
 def get_all_chatrooms():
     logger.info("Get all chatrooms")
@@ -169,7 +180,7 @@ def get_all_chatrooms():
     return render_response(data=chatrooms)
 
 
-@app.route("/chatroom/<id>", methods=['GET'])
+@app.route(f"{PATH_PREFIX}/chatroom/<id>", methods=['GET'])
 @jwt_required()
 def get_chatroom(id):
     logger.info("Get a chatroom")
@@ -178,7 +189,7 @@ def get_chatroom(id):
     return render_response(data=chatroom)
 
 
-@app.route("/chatroom", methods=['POST'])
+@app.route(f"{PATH_PREFIX}/chatroom", methods=['POST'])
 @jwt_required()
 def create_chatroom():
     logger.info("Create a chatroom")
@@ -191,7 +202,7 @@ def create_chatroom():
     return render_response(data=new_room)
 
 
-@app.route("/chatroom/<id>", methods=['DELETE'])
+@app.route(f"{PATH_PREFIX}/chatroom/<id>", methods=['DELETE'])
 @jwt_required()
 def delete_room(id):
     logger.info("Delete a chatroom")
@@ -203,7 +214,8 @@ def delete_room(id):
 
     return render_response()
 
-@app.route("/message", methods=['GET'])
+
+@app.route(f"{PATH_PREFIX}/message", methods=['GET'])
 @jwt_required()
 def get_all_messages():
     logger.info("Get all messages")
@@ -212,7 +224,7 @@ def get_all_messages():
     return render_response(data=messages)
 
 
-@app.route("/message/chatroom/<id>", methods=['GET'])
+@app.route(f"{PATH_PREFIX}/message/chatroom/<id>", methods=['GET'])
 @jwt_required(id)
 def get_all_messages_from_chatroom(id):
     logger.info("Get all messages from specific chatroom")
@@ -220,7 +232,8 @@ def get_all_messages_from_chatroom(id):
     messages = session.query(Message).filter(ChatRoom.id == id).all()
     return render_response(data=messages)
 
-@app.route("/message/user/<id>", methods=['GET'])
+
+@app.route(f"{PATH_PREFIX}/message/user/<id>", methods=['GET'])
 @jwt_required(id)
 def get_all_messages_from_user(id):
     logger.info("Get all messages from specific user")
@@ -228,7 +241,8 @@ def get_all_messages_from_user(id):
     messages = session.query(Message).filter(User.id == id).all()
     return render_response(data=messages)
 
-@app.route("/message/<id>", methods=['GET'])
+
+@app.route(f"{PATH_PREFIX}/message/<id>", methods=['GET'])
 @jwt_required()
 def get_message(id):
     logger.info("Get a message")
@@ -236,7 +250,8 @@ def get_message(id):
     message = session.query(Message).filter(Message.id == id).one()
     return render_response(data=message)
 
-@app.route("/message", methods=['POST'])
+
+@app.route(f"{PATH_PREFIX}/message", methods=['POST'])
 @jwt_required()
 def create_message():
     logger.info("Create a message")
@@ -255,13 +270,20 @@ def create_message():
     return render_response(data=new_message)
 
 
-@app.route("/secret")
+@app.route(f"{PATH_PREFIX}/secret")
 @jwt_required()
 def secret():
     return render_response("Secret!")
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     load_dotenv()
     debug = os.getenv('FLASK_ENV') != 'production'
     port = os.getenv('SC_FLASK_PORT')
+    print(app.url_map)
     app.run('0.0.0.0', port, debug=debug)
